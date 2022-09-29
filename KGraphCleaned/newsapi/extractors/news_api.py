f66bad6625dadc847baf56d74b39cfb69558ae40
Eric H <53405451+EricBCN@users.noreply.github.com>
2022-09-29 18:54:30 +0200

    Add files via upload

--- KGraphCleaned/newsapi/extractors/news_api.py
+++ KGraphCleaned/newsapi/extractors/news_api.py
@@ -28,12 +28,13 @@ auth = HTTPProxyAuth("IMM0632", "GL080817")
 ## ___________ CLASS DEFINITION ___________________
 class NewsAPI(Extractor):
 
-    def __init__(self, language='es', path=DEFAULT_PATH):
+    def __init__(self, language='en', path=DEFAULT_PATH):
 
         super().__init__()
         self.TODAY_DATE = date.today().strftime('%Y-%m-%d')
-        # self.API_KEY = os.getenv('NEWS_API_KEY') or '1c714b6914ee4d6b81cb156511577bc7'
-        self.API_KEY = os.getenv('NEWS_API_KEY') or '66f22d5add7b47c7b984db75104444b1' # Xin's key
+        # self.API_KEY = os.getenv('NEWS_API_KEY') or '66f22d5add7b47c7b984db75104444b1' # Xin's key
+        # self.API_KEY = os.getenv('NEWS_API_KEY') or 'c9c775e2bfcf4537aca5429864f0f3ee'
+        self.API_KEY = os.getenv('NEWS_API_KEY') or 'f461eac24b794c38b77506686f72652d'
         self.WRITE_CSV = False
         self.BASE_URL = os.getenv('NEWS_API_URL_BASE') or 'https://newsapi.org/v2/everything'
         self.DEFAULT_LANGUAGE = language
@@ -42,7 +43,6 @@ class NewsAPI(Extractor):
             self.CONFIG = json.load(file)
 
         self.session = requests.Session()
-
 
     def get_name(self):
         return "newsAPI"
@@ -59,26 +59,22 @@ class NewsAPI(Extractor):
     # Gets a list of news
     def get_dataset(self):
         today = date.today()
-        to_date = today.strftime('%Y-%m-%d')
+        #to_date = today.strftime('%Y-%m-%d')
+        to_date = '2022-09-29'
         # Is Monday?
-        if today.isoweekday() == 1:
-            from_date = (today - timedelta(days=2)).strftime('%Y-%m-%d')
-        else:
-            from_date = (today - timedelta(days=1)).strftime('%Y-%m-%d')
+        from_date = '2022-09-01'
+        # if today.isoweekday() == 1:
+        #     # from_date = (today - timedelta(days=30)).strftime('%Y-%m-%d')
+        #     from_date = (today - timedelta(days=2)).strftime('%Y-%m-%d')
+        # else:
+        #     from_date = (today - timedelta(days=1)).strftime('%Y-%m-%d')
 
         return self.get_dataset_dates(from_date, to_date)
         # End function
 
     # Gets a list of news
-    def get_dataset_4test(self):
-        # he metido del 23,24 marzo
-        return self.get_dataset_dates("2020-04-21", "2020-04-22")
-        # End function
-
-    # Gets a list of news
     def get_dataset_dates(self, from_date, to_date):
         news = list()
-        cont = 0
 
         # 'domains': self.get_domains(self.DEFAULT_LANGUAGE),
         payload = {
@@ -87,49 +83,56 @@ class NewsAPI(Extractor):
             'from': from_date,
             'apiKey': self.API_KEY,
             'excludeDomains': self.get_exclude_domains(self.DEFAULT_LANGUAGE),
-            'sortBy': 'popularity',
-            'pageSize': 100
+            'sortBy': 'popularity'
+            # 'pageSize': 100
         }
 
-        kcore = self.CONFIG[self.DEFAULT_LANGUAGE]['queries']
-        for k in kcore:
-            payload['q'] = k
-            # Result
-            if proxies:
-                result = self.session.get(self.BASE_URL, params=payload, proxies=proxies).json()
-            else:
-                result = self.session.get(self.BASE_URL, params=payload).json()
+        queries = self.CONFIG[self.DEFAULT_LANGUAGE]['queries']
+        entities = self.CONFIG[self.DEFAULT_LANGUAGE]['entities']
+        entities.append("")
 
-            if result.get('status') == 'error':
-                print('Error Message:', result.get('message'))
-            else:
-                total = result.get('totalResults')
-                print(self.CONFIG[self.DEFAULT_LANGUAGE]['ods'][cont] + ': \t', total, ' news')
+        for entity in entities:
+            print(entity)
+            cont = 0
 
-                if result.get('articles'):
-                    for art in result.get('articles'):
-                        article = self._create_article(art, self.DEFAULT_LANGUAGE)
-                        news.append(article)
-            cont += 1
+            for query in queries:
+                payload['q'] = query + " AND " + "(\"" + entity + "\")" if entity != "" else query
+
+                # Result
+                if proxies:
+                    result = self.session.get(self.BASE_URL, params=payload, proxies=proxies).json()
+                else:
+                    result = self.session.get(self.BASE_URL, params=payload).json()
+
+                if result.get('status') == 'error':
+                    print('Error Message:', result.get('message'))
+                else:
+                    total = result.get('totalResults')
+                    print(self.CONFIG[self.DEFAULT_LANGUAGE]['ods'][cont] + ': \t', total, ' news')
+
+                    if result.get('articles'):
+                        for art in result.get('articles'):
+                            article = self._create_article(art=art, language=self.DEFAULT_LANGUAGE, entity=entity)
+                            news.append(article)
+                cont += 1
 
         logger.debug(f'Fetched {len(news)} articles from {from_date} to {to_date}')
         return news
         # End function
-
 
     # Remove duplicated news:
     def reduce_dataset(self, dataset):
         filtered_dataset = []
         inserted_urls = set()
         for art in dataset:
-            if art['url'] not in inserted_urls:
+            url = art['url'] if len(art['entity']) == 0 else art['entity'][0] + art['url']
+            if url not in inserted_urls:
                 filtered_dataset.append(art)
-                inserted_urls.add(art['url'])
+                inserted_urls.add(url)
 
         # Remove the articles already stored on the database from the dataset
         print('dataset size: ', len(filtered_dataset))
         return filtered_dataset
-
 
     ##################################
 
@@ -161,12 +164,10 @@ class NewsAPI(Extractor):
 
 
     # Create Article
-    def _create_article(self, art, language):
-
+    def _create_article(self, art, language, entity):
         if (art.get('publishedAt') is not None)  and len(art['publishedAt']) > 0:
             dt_utc = datetime.datetime.strptime(art.get('publishedAt'), "%Y-%m-%dT%H:%M:%SZ")
             published_at = dt_utc.strftime(Constants.DATE_FORMAT)
-            #published_at = self.date_from_news_api(art.get('publishedAt'))
         else:
             dt_utc = datetime.datetime.utcnow()
             published_at = dt_utc.strftime(Constants.DATE_FORMAT)
@@ -184,9 +185,7 @@ class NewsAPI(Extractor):
         article['tags'] = dict()
         article['content'] = 'N/A'
         article['published_at'] = dt_utc
-        return article
+        # article['entity'] = [entity] if entity != "" else []
+        article['entity'] = [entity.lower()] if entity != "" else []
 
-    # Transform input date to extractors format. "2020-02-12T00:00:00Z" to "26/03/2020 10:29:34"
-    def date_from_news_api(self, input_date):
-        d1 = datetime.datetime.strptime(input_date, "%Y-%m-%dT%H:%M:%SZ")
-        return d1.strftime(Constants.DATE_FORMAT)
+        return article
